67027bb7d047c5ad5dffad6334b1521895fa1858
Rob Rogers <rob.rogers@me.com>
2017-07-27 23:25:28 -0700

    test case simplified

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,43 +13,5 @@ abstract class TestCase extends BaseTestCase
     protected function setUp()
     {
         parent::setUp();
-        //$this->disableExceptionHandling();
-    }
-
-    protected function signIn($user = null)
-    {
-        if (!$user) {
-            $user = factory('App\User')->create();
-        }
-
-        $this->be($user);
-
-        return $this;
-    }
-
-
-    // Hat tip, @adamwathan.
-    protected function disableExceptionHandling()
-    {
-        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-
-            public function report(\Exception $e) {}
-
-            public function render($request, \Exception $e) {
-                throw $e;
-            }
-        });
-
-        return $this;
-    }
-
-    protected function withExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
-
-        return $this;
     }
 }
