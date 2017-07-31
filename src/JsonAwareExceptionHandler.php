c166bf1ad3c949f1e062d02e2f6cc43091d23812
Rob Rogers <rob.rogers@me.com>
2017-07-31 02:52:08 -0700

    for some reason laravel does not have a unauth method in the base handler. learn somethnig new everyday

--- src/JsonAwareExceptionHandler.php
+++ src/JsonAwareExceptionHandler.php
@@ -4,6 +4,7 @@ namespace RobRogers3\LaravelExceptionHandler;
 
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as BaseExceptionHandler;
+use \Illuminate\Auth\AuthenticationException;
 
 class JsonAwareExceptionHandler extends BaseExceptionHandler
 {
@@ -22,5 +23,21 @@ class JsonAwareExceptionHandler extends BaseExceptionHandler
         }
         
         return parent::render($request, $exception);
+    }
+
+        /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response('Sorry you are Unauthenticated.', 401);
+        }
+
+        return redirect()->guest('login');
     }
 }
